b104b53076075ac97fbeeaf1b9290d3a47eaf785
Shem Magnezi <shem@Shems-MacBook-Pro.local>
2017-11-29 21:05:59 +0200

    event type

---
+++ src/components/event.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const EventCard = ( event ) => (
+  <div className="content" style={{ border: '1px solid #eaecee', padding: '2em 4em' }} key={event.id}>
+    <p>
+      <Link className="has-text-primary" to={event.frontmatter.path}>
+        {event.frontmatter.title}
+      </Link>
+      <span> &bull; </span>
+      <small>{event.frontmatter.date}</small>
+    </p>
+    <p>
+      {event.excerpt}
+      <br />
+      <br />
+      <Link className="button is-small" to={event.frontmatter.path}>
+        Keep Reading →
+      </Link>
+    </p>
+  </div>
+);
+
+export default FeatureGrid;
