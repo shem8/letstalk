b596f09a3a5ac9c1402385ccb76eb64b77f6ee20
Shem Magnezi <shem@Shems-MacBook-Pro.local>
2017-12-01 22:20:29 +0200

    event schema + basic card

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -2,23 +2,55 @@ import React from 'react';
 import Link from 'gatsby-link';
 
 const EventCard = ({ event }) => (
-  <div className="content" style={{ border: '1px solid #eaecee', padding: '2em 4em' }} key={event.id}>
-    <p>
-      <Link className="has-text-primary" to={event.frontmatter.path}>
-        {event.frontmatter.title}
-      </Link>
-      <span> &bull; </span>
-      <small>{event.frontmatter.date}</small>
-    </p>
-    <p>
-      {event.excerpt}
-      <br />
-      <br />
-      <Link className="button is-small" to={event.frontmatter.path}>
-        Keep Reading →
-      </Link>
-    </p>
+  <Link to={event.frontmatter.path}>
+  <div className="card" key={event.id}>
+    <div className="card-content">
+      <div className="card-header">
+        <div className="card-header-title">
+          <p class="title">
+              {event.frontmatter.title}
+            <span> &bull; </span>
+            <small>{event.frontmatter.date}</small>
+          </p>
+          <p class="subtitle">
+            {event.frontmatter.subtitle}
+          </p>
+          <p class="location">
+            {event.frontmatter.location}
+          </p>
+        </div>
+        <div className="card-header-dates">
+          <p className="card-header-dates-event">
+            Event date: {event.frontmatter.date}
+          </p>
+          <p className="card-header-dates-cfp">
+            CFP deadline: {event.frontmatter.deadline}
+          </p>
+        </div>
+      </div>
+      <p>
+        {event.excerpt}
+        <br/>
+        <a class="button is-primary" href={event.frontmatter.cfp_link}>
+         Submit
+        </a>
+        <a class="button is-info" href={event.frontmatter.event_link}>
+         Event homepage
+        </a>
+        <a class="button is-small" href={event.frontmatter.facebook}>
+         Facebook
+        </a>
+        <a class="button is-small" href={event.frontmatter.twitter}>
+         Twitter
+        </a>
+      </p>
+    </div>
   </div>
+  </Link>
 );
 
 export default EventCard;
+
+
+  // - {label: "Travel expenses", name: "expenses", widget: "boolean"}
+  // - {label: "Travel expenses description", name: "expenses_desc", widget: "text"}
--- src/pages/event/2017-11-29-test-event.md
+++
@@ -1,7 +0,0 @@
----
-templateKey: event-page
-path: test
-title: Test event
-date: 2017-11-29T19:09:48.022Z
----
-Bla bla bla
