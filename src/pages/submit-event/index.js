afcc9cfbfdac930831164375595d6dffb9753036
Shem Magnezi <shem@Shems-MacBook-Pro.local>
2017-12-15 17:44:33 +0200

    fix form

--- src/pages/submit-event/index.js
+++ src/pages/submit-event/index.js
@@ -1,98 +1,138 @@
 import React from 'react';
 
-const SubmitEvent = ({ active }) => (
-  <form className="section" name="submit-event" netlify>
-    <div className="field">
-      <label className="label">Event Title</label>
-      <div className="control">
-        <input className="input" type="text"></input>
-      </div>
-    </div>
+function encode(data) {
+  return Object.keys(data)
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .join("&");
+}
 
-    <div className="field">
-      <label className="label">Subtitle</label>
-      <div className="control">
-        <input className="input" type="text"></input>
-      </div>
-    </div>
+export default class SubmitEvent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-    <div className="field">
-      <label className="label">Event description</label>
-      <div className="control">
-        <textarea className="textarea"></textarea>
-      </div>
-    </div>
+  handleChange = (e) => {
+    this.setState({[e.target.name]: e.target.value});
+  }
 
-    <div className="field">
-      <label className="label">Event date</label>
-      <div className="control">
-        <input className="input" type="date"></input>
-      </div>
-    </div>
+  handleSubmit = e => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...this.state })
+    })
+      .then(() => alert("Success!"))
+      .catch(error => alert(error));
 
-    <div className="field">
-      <label className="label">CFP deadline</label>
-      <div className="control">
-        <input className="input" type="date"></input>
-      </div>
-    </div>
+    e.preventDefault();
+  };
 
-    <div className="field">
-      <label className="label">Location</label>
-      <div className="control">
-        <input className="input" type="text"></input>
-      </div>
-    </div>
+render() {
+  return (
+      <form
+        className="section"
+        name="submit-event"
+        method="post"
+        action="/thanks/"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+        onSubmit={this.handleSubmit}>
 
-    <div className="field">
-      <div className="control">
-        <label className="checkbox">
-          <input type="checkbox"/> Travel expenses
+        <label hidden>
+          Don’t fill this out: <input name="bot-field" />
         </label>
-      </div>
-    </div>
+        <div className="field">
+          <label className="label">Event Title</label>
+          <div className="control">
+            <input className="input" type="text" onChange={this.handleChange}></input>
+          </div>
+        </div>
 
-    <div className="field">
-      <label className="label">Travel expenses cover</label>
-      <div className="control">
-        <textarea className="textarea" rows="2"></textarea>
-      </div>
-    </div>
+        <div className="field">
+          <label className="label">Subtitle</label>
+          <div className="control">
+            <input className="input" type="text" onChange={this.handleChange}></input>
+          </div>
+        </div>
 
-    <div className="field">
-      <label className="label">Event link</label>
-      <div className="control">
-        <input className="input" type="text"></input>
-      </div>
-    </div>
+        <div className="field">
+          <label className="label">Event description</label>
+          <div className="control">
+            <textarea className="textarea" onChange={this.handleChange}></textarea>
+          </div>
+        </div>
 
-    <div className="field">
-      <label className="label">CFP link</label>
-      <div className="control">
-        <input className="input" type="text"></input>
-      </div>
-    </div>
+        <div className="field">
+          <label className="label">Event date</label>
+          <div className="control">
+            <input className="input" type="date" onChange={this.handleChange}></input>
+          </div>
+        </div>
 
-    <div className="field">
-      <label className="label">Twitter link</label>
-      <div className="control">
-        <input className="input" type="text"></input>
-      </div>
-    </div>
+        <div className="field">
+          <label className="label">CFP deadline</label>
+          <div className="control">
+            <input className="input" type="date" onChange={this.handleChange}></input>
+          </div>
+        </div>
 
-    <div className="field">
-      <label className="label">Facebook page</label>
-      <div className="control">
-        <input className="input" type="text"></input>
-      </div>
-    </div>
+        <div className="field">
+          <label className="label">Location</label>
+          <div className="control">
+            <input className="input" type="text" onChange={this.handleChange}></input>
+          </div>
+        </div>
 
-    <div className="field">
-      <div className="control">
-        <button className="button is-link" type="submit">Submit</button>
-      </div>
-    </div>
-  </form>
-);
+        <div className="field">
+          <div className="control">
+            <label className="checkbox">
+              <input type="checkbox" onChange={this.handleChange}/> Travel expenses
+            </label>
+          </div>
+        </div>
 
-export default SubmitEvent;
+        <div className="field">
+          <label className="label">Travel expenses cover</label>
+          <div className="control">
+            <textarea className="textarea" rows="2" onChange={this.handleChange}></textarea>
+          </div>
+        </div>
+
+        <div className="field">
+          <label className="label">Event link</label>
+          <div className="control">
+            <input className="input" type="text" onChange={this.handleChange}></input>
+          </div>
+        </div>
+
+        <div className="field">
+          <label className="label">CFP link</label>
+          <div className="control">
+            <input className="input" type="text" onChange={this.handleChange}></input>
+          </div>
+        </div>
+
+        <div className="field">
+          <label className="label">Twitter link</label>
+          <div className="control">
+            <input className="input" type="text" onChange={this.handleChange}></input>
+          </div>
+        </div>
+
+        <div className="field">
+          <label className="label">Facebook page</label>
+          <div className="control">
+            <input className="input" type="text" onChange={this.handleChange}></input>
+          </div>
+        </div>
+
+        <div className="field">
+          <div className="control">
+            <button className="button is-link" type="submit">Submit</button>
+          </div>
+        </div>
+      </form>
+    );
+  }
+}
